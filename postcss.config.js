5cb7b08c2442b963e435d0f97fd54e3234d1187a
truefusion <truefusion@gmail.com>
2024-06-13 16:35:16 -0400

    Init.

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+export default {
+	tailwindcss: {},
+	autoprefixer: {},
+	'postcss-font-magician': {
+		variants: {
+			'Roboto': {
+				'100': [],
+				'300': [],
+				'400': [],
+				'500': [],
+				'700': [],
+				'900': [],
+			},
+			'Roboto Condensed': {
+				'100': [],
+				'300': [],
+				'400': [],
+				'500': [],
+				'700': [],
+				'900': [],
+			},
+		},
+		foundries: ['google']
+	},
+}
