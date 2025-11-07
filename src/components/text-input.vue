ef78f8ff5983e712e40353d5b4032dfde2a7a321
AimForNaN <aimfornan@localhost>
2025-11-07 07:13:24 +0000

    Text input component.

---
+++ src/components/text-input.vue
@@ -0,0 +1,11 @@
+<script setup>
+	const model = defineModel();
+	const props = defineProps({
+		is: String,
+	});
+</script>
+
+<template>
+	<input v-if="props.is === 'input'" v-model="model">
+	<textarea v-else-if="props.is === 'textarea'" v-model="model"></textarea>
+</template>
