562465d5b1962c5900a283b2b7fe0dc9a866ea5c
AimForNaN <aimfornan@localhost>
2025-11-17 13:33:10 +0000

    Formatting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
-
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
